24dffe770d369e580c4657c0c00302990841f0c3
adamb06 <adamb06@7c4ca305-8530-0410-9e1e-bfd01828a707>
2008-10-28 21:25:10 +0000

    added unix version of install script

---
+++ src/main/bin/install.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+###################
+# Make sure Apache Maven2 is installed
+###################
+
+checkMavenHome()
+{
+  if [ ! -f ${MAVEN_HOME}/bin/mvn ] ; then
+   echo "Apache Maven 2 was not found at the location specified by the MAVEN_HOME variable. Please make sure MAVEN_HOME is set correctly".
+   exit 1
+  fi
+}
+
+###################
+# Make sure DUST_HOME variable is set
+###################
+
+checkDustHome() 
+{
+ if [ "${DUST_HOME}" = "" ] ; then
+  echo "You must set the DUST_HOME environment variable to point to "
+  echo "into which you checked out DUST from subversion."
+  DYNAMO_HOME=.
+  DYNAMO_ROOT=..
+  exit 1
+ fi
+
+}
+
+###################
+# Make sure the DYNAMO_HOME variable is set
+###################
+
+checkDynamoHome()
+{
+  if [ "${DYNAMO_HOME}" = "" ] ; then
+    echo "You must set the DYNAMO_HOME environment variable to point to your"
+    echo "ATG install directory."
+    DYNAMO_HOME=.
+    DYNAMO_ROOT=..
+    exit 1
+  else
+    #
+    # Set DYNAMO_ROOT to be the directory above DYNAMO_HOME
+    #
+  DYNAMO_ROOT=${DYNAMO_HOME}/..
+  fi
+}
+
+###################
+# Actually invoke maven and install libraries to the local repository
+###################
+
+doInstall()
+{
+  pushd ${DUST_HOME}
+  ${MAVEN_HOME}/bin/mvn install:install-file -DgroupId=atg -DartifactId=das-resources -Dversion=2007.1p1 -Dpackaging=jar -Dfile=${DYNAMO_HOME}/../DAS/lib/resources.jar
+  ${MAVEN_HOME}/bin/mvn install:install-file -DgroupId=atg -DartifactId=das -Dversion=2007.1p1 -Dpackaging=jar -Dfile=${DYNAMO_HOME}/../DAS/lib/classes.jar
+  popd
+}
+
+###################
+# Prints a success message
+###################
+
+success() 
+{
+ echo "ATG libraries installed. You may not build ATG DUST by typing 'mvn install'."
+}
+
+###################
+# Main Script
+###################
+
+checkMavenHome
+checkDustHome
+checkDynamoHome
+doInstall
+success
