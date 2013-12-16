089fd64bd8019e3fa606c6b9cb6571ca9187f4fb
Matt Sicker <matthew.sicker@ahold.com>
2013-12-15 20:07:32 -0600

    Add module list to @Nuke.

--- src/main/java/atg/tools/dynunit/Nuke.java
+++ src/main/java/atg/tools/dynunit/Nuke.java
@@ -2,28 +2,33 @@ package atg.tools.dynunit;
 
 import javax.inject.Qualifier;
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Injection annotation marking a field to be injected with a Nucleus instance. Unit tests using this annotation on a
- * public Nucleus field should initialize the injections via {@code DynUnit.init(this)}.
+ * Injection qualifier for a Nucleus instance. This annotation should be used on the test class itself or on a
+ * {@link atg.nucleus.Nucleus Nucleus} field. The class should use {@link atg.tools.dynunit.DynUnit#init(Object)} to
+ * inject the Nucleus along with any Nucleus-registered components.
  *
  * @author msicker
  * @version 1.0.0
  */
 @Documented
 @Retention(RUNTIME)
-@Target({ FIELD, METHOD })
+@Target({ TYPE, FIELD })
 @Qualifier
+@Inherited
 public @interface Nuke {
 
     /**
      * Root ATG-Config-Path to use for Nucleus instance.
      */
     public String value();
+
+    public String[] modules() default { };
 }
