73754801368cbccd52b9dbc6da663dccf29da58b
Matt Sicker <matthew.sicker@ahold.com>
2013-12-15 20:04:09 -0600

    Update DynUnit base class.

--- src/main/java/atg/tools/dynunit/DynUnit.java
+++ src/main/java/atg/tools/dynunit/DynUnit.java
@@ -3,6 +3,8 @@ package atg.tools.dynunit;
 import atg.nucleus.Nucleus;
 import atg.nucleus.ServiceException;
 import atg.tools.dynunit.inject.NucleusInjectorFactory;
+import atg.tools.dynunit.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,7 +15,12 @@ import java.lang.reflect.Field;
  * @author msicker
  * @version 1.0.0
  */
-public class DynUnit {
+public final class DynUnit {
+
+    public static final String DYNUNIT_HOME_PROPERTY = "atg.tools.dynunit.home";
+
+    private DynUnit() {
+    }
 
     private static final Logger logger = LogManager.getLogger();
 
@@ -50,6 +57,18 @@ public class DynUnit {
             logger.error("Couldn't find nucleus field to stop!");
         }
         logger.exit();
+    }
+
+    public static String getHome() {
+        logger.entry();
+        String dynUnitHome = PropertiesUtil.getSystemProperty(DYNUNIT_HOME_PROPERTY);
+        if (StringUtils.isEmpty(dynUnitHome)) {
+            dynUnitHome = System.getenv("DYNUNIT_HOME");
+            if (StringUtils.isEmpty(dynUnitHome)) {
+                logger.error("Can't find DynUnit home directory. Please set the {} property.", DYNUNIT_HOME_PROPERTY);
+            }
+        }
+        return logger.exit(StringUtils.defaultString(dynUnitHome));
     }
 
 }
